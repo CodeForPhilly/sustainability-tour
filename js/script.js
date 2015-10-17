4156bf1e41c40234225293b4d0f66e5e21bbab11
Tyler <jtwiest@gmail.com>
2015-10-17 13:53:21 -0400

    Added map and points from geojson file.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
-<script>
+$(document).ready(function() {
+    var map; 
 
-</script>
+    $.getJSON('GSI_Tour_Content.geojson', function(data) {                 
+        
+        // Generate Map
+        map = L.map('map').setView([39.9394357303,-75.15820773], 13);
+
+        // Create a data point for each marker
+        L.geoJson(data, {
+            
+            onEachFeature: function (feature, layer) {
+                layer.bindPopup('<a href="#' + encodeURIComponent(feature.properties['Site Name']) + '">' + feature.properties['Site Name'] + "</a>"
+                );
+            }
+        }).addTo(map);
+
+
+        L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+        	subdomains: 'abcd',
+        	maxZoom: 19
+        }).addTo(map);
+    
+    });
+});
